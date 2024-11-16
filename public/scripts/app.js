0b06cea92f23a86339674f00c26cfeb12ff8017d
potatodevsr <hoshiku1997@gmail.com>
2024-11-16 21:26:54 +0700

    JSX Expressions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,16 @@
 "use strict";
 
 console.log('App.js is running!');
-var template = /*#__PURE__*/React.createElement("h1", null, "Indecicision App");
+// JSX - Javascript XML
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Indecicision App"), /*#__PURE__*/React.createElement("p", null, "This is some info"), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "Item One"), /*#__PURE__*/React.createElement("li", null, "Item Two")));
+var userName = 'Chadapohn';
+var userAge = 27;
+var userLocation = 'Thailand';
+var user = {
+  name: 'Chadapohn',
+  age: 26,
+  location: 'Thailand'
+};
+var templateTwo = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, user.name), /*#__PURE__*/React.createElement("p", null, "Age: ", user.age), /*#__PURE__*/React.createElement("p", null, "Location: ", user.location));
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,40 @@
 console.log('App.js is running!');
+// JSX - Javascript XML
+var template = (
+    <div>
+        <h1>Indecicision App</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+        </ol>
+    </div>
+);
 
-var template = <h1>Indecicision App</h1>
+var userName = 'Chadapohn';
+var userAge = 27;
+var userLocation = 'Thailand';
+
+var user = {
+    name: 'Chadapohn',
+    age: 26,
+    location: 'Thailand'
+}
+
+var templateTwo = (
+    <div>
+        <h1>
+            {user.name}
+        </h1>
+        <p>
+            Age: {user.age}
+        </p>
+        <p>
+            Location: {user.location}
+        </p>
+    </div>
+)
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
