deb0683b4b7b7e034bc666072d6197743d9ef974
potatodevsr <hoshiku1997@gmail.com>
2024-11-16 23:11:51 +0700

    Conditional Rendering in JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,15 +2,22 @@
 
 console.log('App.js is running!');
 // JSX - Javascript XML
-var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Indecicision App"), /*#__PURE__*/React.createElement("p", null, "This is some info"), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "Item One"), /*#__PURE__*/React.createElement("li", null, "Item Two")));
-var userName = 'Chadapohn';
-var userAge = 27;
-var userLocation = 'Thailand';
+var app = {
+  title: 'Some title',
+  subtitle: 'This is my subtitle',
+  options: ['One', 'Two']
+};
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, app.title), app.subtitle && /*#__PURE__*/React.createElement("p", null, app.subtitle), /*#__PURE__*/React.createElement("p", null, " ", app.options.length > 0 ? 'Here are your options' : 'No options'), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "Item One"), /*#__PURE__*/React.createElement("li", null, "Item Two")));
 var user = {
   name: 'Chadapohn',
   age: 26,
   location: 'Thailand'
 };
-var templateTwo = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, user.name), /*#__PURE__*/React.createElement("p", null, "Age: ", user.age), /*#__PURE__*/React.createElement("p", null, "Location: ", user.location));
+function getLocation(location) {
+  if (location) {
+    return /*#__PURE__*/React.createElement("p", null, "Location: ", location);
+  }
+}
+var templateTwo = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, user.name ? user.name : 'Anonymous'), user.age >= 18 && /*#__PURE__*/React.createElement("p", null, "Age: ", user.age), getLocation(user.location));
 var appRoot = document.getElementById('app');
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,19 +1,28 @@
 console.log('App.js is running!');
 // JSX - Javascript XML
+var app = {
+    title: 'Some title',
+    subtitle: 'This is my subtitle',
+    options: ['One', 'Two']
+}
+
 var template = (
     <div>
-        <h1>Indecicision App</h1>
-        <p>This is some info</p>
+        <h1>
+            {app.title}
+        </h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p> {app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
         <ol>
-            <li>Item One</li>
-            <li>Item Two</li>
+            <li>
+                Item One
+            </li>
+            <li>
+                Item Two
+            </li>
         </ol>
-    </div>
-);
-
-var userName = 'Chadapohn';
-var userAge = 27;
-var userLocation = 'Thailand';
+    </div >
+)
 
 var user = {
     name: 'Chadapohn',
@@ -21,20 +30,24 @@ var user = {
     location: 'Thailand'
 }
 
+function getLocation(location) {
+    if (location) {
+        return <p>
+            Location: {location}
+        </p>;
+    }
+}
+
 var templateTwo = (
     <div>
         <h1>
-            {user.name}
+            {user.name ? user.name : 'Anonymous'}
         </h1>
-        <p>
-            Age: {user.age}
-        </p>
-        <p>
-            Location: {user.location}
-        </p>
+        {user.age >= 18 && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 )
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
