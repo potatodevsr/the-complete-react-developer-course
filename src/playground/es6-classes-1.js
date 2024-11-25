d2a0f0432b89832233e238d68b797a3857c9c045
potatodevsr <hoshiku1997@gmail.com>
2024-11-25 14:32:30 +0700

    ES6 Classes: Part II

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -14,8 +14,44 @@ class Person {
     }
 }
 
-const me = new Person('Chadapohn Sorakanit', 26);
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+
+        if (this.hasMajor()) {
+            description += `Their major is ${this.major}.`;
+        }
+
+        return description;
+        // return 'testing';
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    getGretting() {
+        let greeting = super.getGretting();
+
+        if (this.homeLocation) {
+            greeting += `I am visiting from ${this.homeLocation}`;
+        }
+
+        return greeting;
+    }
+}
+
+const me = new Student('Chadapohn Sorakanit', 26, 'Computer Science');
 console.log(me.getDescription());
 
-const other = new Person();
+const other = new Traveler(undefined, undefined, 'Nowhere');
 console.log(other.getDescription());
