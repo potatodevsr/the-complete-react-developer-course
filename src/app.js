9dd5c678dc6e2ef80552936dae5ad4c509908ce9
potatodevsr <hoshiku1997@gmail.com>
2024-11-27 11:35:48 +0700

    Creating a React Component

--- src/app.js
+++ src/app.js
@@ -1,117 +1,53 @@
-console.log('App.js is running!');
-// JSX - Javascript XML
-const app = {
-    title: 'Some title',
-    subtitle: 'This is my subtitle',
-    options: []
-};
-
-const onFormSubmit = (e) => {
-    e.preventDefault();
-    const option = e.target.elements.option.value;
-
-    if (option) {
-        app.options.push(option);
-        e.target.elements.option.value = '';
-        render(); // Call the correct render function
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Pput your life in the hands of a computer</h2>
+            </div>
+        )
     }
-};
-// onRemoveAll
-const onRemoveAll = () => {
-    app.options = [];
-    render(); // Call the correct render function
-};
 
-const onMakeDecision = () => {
-    const randomNum = Math.floor(Math.random() * app.options.length); // Fixed the typo
-    const option = app.options[randomNum];
-    alert(option);
-    console.log(randomNum);
-};
-
-const appRoot = document.getElementById('app');
-const render = () => {
-    const template = (
-        <div>
-            <h1>{app.title}</h1>
-            {app.subtitle && <p>{app.subtitle}</p>}
-            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-            <p>Total options: {app.options.length}</p>
-            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What Should I do?</button>
-            <button onClick={onRemoveAll}>Remove All</button>
-
-            <ol>
-                {app.options.map((option) => (
-                    <li key={option}>Option: {option}</li>
-                ))}
-            </ol>
-            <form onSubmit={onFormSubmit}>
-                <input type="text" name="option" />
-                <button>Add Option</button>
-            </form>
-        </div>
-    );
-
-    ReactDOM.render(template, appRoot);
-};
-
-// Initial render
-render();
-
-
-
-const user = {
-    name: 'Chadapohn',
-    age: 26,
-    location: 'Thailand'
 }
 
-function getLocation(location) {
-    if (location) {
-        return <p>
-            Location: {location}
-        </p>;
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        )
     }
 }
 
-// const templateTwo = (
-//     <div>
-//         <h1>
-//             {user.name ? user.name : 'Anonymous'}
-//         </h1>
-//         {user.age >= 18 && <p>Age: {user.age}</p>}
-//         {getLocation(user.location)}
-//     </div>
-// )
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                Options Component here
+            </div>
+        )
+    }
+}
 
-// let count = 0;
-// const addOne = () => {
-//     count++;
-//     renderCounterApp();
-// };
-// const minusOne = () => {
-//     count--;
-//     renderCounterApp();
-// };
-// const reset = () => {
-//     count = 0;
-//     renderCounterApp();
-// };
-// const renderCounterApp = () => {
-//     const templateTwo = (
-//         <div>
-//             <h1>Count: {count}</h1>
-//             <button onClick={addOne}>+1</button>
-//             <button onClick={minusOne}>-1</button>
-//             <button onClick={reset}>Reset</button>
-//         </div>
-//     );
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                AddOption Component here
+            </div>
+        )
+    }
+}
 
-//     // const appRoot = document.getElementById('app');
+const jsx = (
+    <div>
+        <h1>Title</h1>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+    </div>
 
-//     // ReactDOM.render(templateTwo, appRoot);
-// }
-
-// const appRoot = document.getElementById('app');
-// ReactDOM.render(template, appRoot);
-// renderCounterApp();
+)
+ReactDOM.render(jsx, document.getElementById('app'));
